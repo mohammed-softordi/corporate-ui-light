d7e042d6c54110850178d7e6df275c41a42a53ae
mohammed-softordi <mohammed.ayowa@softordi.com>
2019-05-03 16:03:37 +0200

    IE mobile device fix

--- polyfill/ie-polyfills.js
+++ polyfill/ie-polyfills.js
@@ -74,3 +74,75 @@ if (!Element.prototype.toggleAttribute) {
         }
     };
 }
+/*
+ * @function
+ * @name Object.prototype.inArray
+ * @description Extend Object prototype within inArray function
+ *
+ * @param {mix}    needle       - Search-able needle
+ * @param {bool}   searchInKey  - Search needle in keys?
+ *
+ */
+Object.defineProperty(Object.prototype, 'inArray',{
+    value: function(needle, searchInKey, ignore, property){
+
+        var object = this, ignoredProps = {}, searchProperty = {};
+
+        if(ignore){
+            ignoredProps = needle[ignore];
+        }
+        if(property){
+            searchProperty = needle[property];
+        }
+        if( Object.prototype.toString.call(needle) === '[object Object]' ||
+            Object.prototype.toString.call(needle) === '[object Array]'){
+            needle = JSON.stringify(needle);
+        }
+
+        return Object.keys(object).some(function(key){
+
+            var value = object[key];
+
+            if(ignore){
+                value[ignore] = ignoredProps;
+            }
+            if(property){
+                if (value[property] == searchProperty){
+                    return true;
+                }
+            }
+            if( Object.prototype.toString.call(value) === '[object Object]' ||
+                Object.prototype.toString.call(value) === '[object Array]'){
+                value = JSON.stringify(value);
+            }
+
+            if(searchInKey){
+                if(value === needle || key === needle){
+                    return true;
+                }
+            }else{
+                if(value === needle){
+                    return true;
+                }
+            }
+        });
+    },
+    writable: true,
+    configurable: true,
+    enumerable: false
+});
+(function () {
+
+    if ( typeof window.CustomEvent === "function" ) return false;
+
+    function CustomEvent ( event, params ) {
+        params = params || { bubbles: false, cancelable: false, detail: null };
+        var evt = document.createEvent( 'CustomEvent' );
+        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+        return evt;
+    }
+
+    CustomEvent.prototype = window.Event.prototype;
+
+    window.CustomEvent = CustomEvent;
+})();
